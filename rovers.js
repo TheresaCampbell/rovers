b27dc3dfab087fcb353b374506373963a55b3961
Theresa Campbell <t.campbell@sasktel.net>
2018-09-30 19:29:45 +0000

    Refactored by putting the input array into a global variable, allowing for modular functions.

--- rovers.js
+++ rovers.js
@@ -1,24 +1,27 @@
 const fs = require("fs");
 
-fs.readFile('directions.txt', (err, data) => {
-  if (err) throw err;
+const input = fs.readFileSync('directions.txt').toString().split('\n');
+console.log("Array of Instructions: ", input);
 
-  const input = data.toString().split('\n');
-  console.log("Array of Instructions: ", input);
+const maxPositionX = input[0].split(' ')[0];
 
-  const maxPositionX = input[0].split(' ')[0];
-  console.log("Max X Position: ", maxPositionX);
+const maxPositionY = input[0].split(' ')[1];
 
-  const maxPositionY = input[0].split(' ')[1];
-  console.log("Max Y Position: ", maxPositionY);
+let x = Number(input[1].split(' ')[0]);
+let y = Number(input[1].split(' ')[1]);
+let startDirection = input[1].split(' ')[2];
+console.log("X: ", x, " Y: ", y, " Starting Direction: ", startDirection);
 
-  let x = Number(input[1].split(' ')[0]);
-  let y = Number(input[1].split(' ')[1]);
-  let startDirection = input[1].split(' ')[2];
-  console.log("X: ", x, " Y: ", y, " Starting Direction: ", startDirection);
+const instructions = input[2].split("");
+console.log("Instructions: ", instructions);
 
-  const instructions = input[2].split("");
-  console.log("Instructions: ", instructions);
+const compass = ['N', 'E', 'S', 'W'];
 
+let compassIndex = '';
 
-})
+for(i = 0; i < compass.length; i++) {
+  if (compass[i] === startDirection) {
+    compassIndex = i;
+  }
+}
+console.log("Compass Index: ", compassIndex);
