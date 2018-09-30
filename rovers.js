1e16d9aa293ec7de3f46857557c614635d92f4fd
Theresa Campbell <t.campbell@sasktel.net>
2018-09-30 19:08:23 +0000

    Refactoring.

--- rovers.js
+++ rovers.js
@@ -6,21 +6,15 @@ fs.readFile('directions.txt', (err, data) => {
   const input = data.toString().split('\n');
   console.log("Array of Instructions: ", input);
 
-  const maxPosition = input[0].split(' ');
-  console.log("Max Position on Grid: ", maxPosition);
-
-  const maxPositionX = maxPosition[0];
+  const maxPositionX = input[0].split(' ')[0];
   console.log("Max X Position: ", maxPositionX);
 
-  const maxPositionY = maxPosition[1];
+  const maxPositionY = input[0].split(' ')[1];
   console.log("Max Y Position: ", maxPositionY);
 
-  const startPosition = input[1].split(' ');
-  console.log("Start Position: ", startPosition);
-
-  let x = Number(startPosition[0]);
-  let y = Number(startPosition[1]);
-  let startDirection = startPosition[2];
+  let x = Number(input[1].split(' ')[0]);
+  let y = Number(input[1].split(' ')[1]);
+  let startDirection = input[1].split(' ')[2];
   console.log("X: ", x, " Y: ", y, " Starting Direction: ", startDirection);
 
   const instructions = input[2].split("");
