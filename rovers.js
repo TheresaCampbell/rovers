878b55af25e2f1777d217f9a0feed9815bc2393c
Theresa Campbell <t.campbell@sasktel.net>
2018-09-30 18:59:21 +0000

    Printing input in separate variables so that different pieces of input can be manipulated individually.

--- rovers.js
+++ rovers.js
@@ -3,6 +3,28 @@ const fs = require("fs");
 fs.readFile('directions.txt', (err, data) => {
   if (err) throw err;
 
-  const array = data.toString().split('\n');
-  console.log("Array of directions: ", array);
+  const input = data.toString().split('\n');
+  console.log("Array of Instructions: ", input);
+
+  const maxPosition = input[0].split(' ');
+  console.log("Max Position on Grid: ", maxPosition);
+
+  const maxPositionX = maxPosition[0];
+  console.log("Max X Position: ", maxPositionX);
+
+  const maxPositionY = maxPosition[1];
+  console.log("Max Y Position: ", maxPositionY);
+
+  const startPosition = input[1].split(' ');
+  console.log("Start Position: ", startPosition);
+
+  let x = Number(startPosition[0]);
+  let y = Number(startPosition[1]);
+  let startDirection = startPosition[2];
+  console.log("X: ", x, " Y: ", y, " Starting Direction: ", startDirection);
+
+  const instructions = input[2].split("");
+  console.log("Instructions: ", instructions);
+
+
 })
