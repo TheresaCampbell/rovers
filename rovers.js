c63cb11c7c7de2d94ba5053ed3eec65f6e309b5f
Theresa Campbell <t.campbell@sasktel.net>
2018-09-30 18:48:42 +0000

    Installed mocha and chai for testfor testing. Created a file for tests.

--- rovers.js
+++ rovers.js
@@ -4,5 +4,5 @@ fs.readFile('directions.txt', (err, data) => {
   if (err) throw err;
 
   const array = data.toString().split('\n');
-  console.log(array);
+  console.log("Array of directions: ", array);
 })
