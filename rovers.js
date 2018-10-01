93c275f30e2fcfa4ef00743ccbca6375e4951083
Theresa Campbell <t.campbell@sasktel.net>
2018-10-01 19:42:14 +0000

    Simplified code to handle two rovers in the same way it previously handled one.

--- rovers.js
+++ rovers.js
@@ -1,76 +1,69 @@
+// Sample Rover Directions: [ '1 2 N', 'LMLMLMLMM' ]
+
 const fs = require("fs");
 const input = fs.readFileSync('directions.txt').toString().split('\n');
-console.log("Input: ", input);
 
-const maxPositionX = input[0].split(' ')[0];
-const maxPositionY = input[0].split(' ')[1];
-
-const roverOne = input.slice(1, 3);
-const roverTwo = input.slice(3, 5);
-
-let rovers = [roverOne, roverTwo];
+const maxPosition = input[0].split(' ');
+const maxPositionX = maxPosition[0];
+const maxPositionY = maxPosition[1];
+const rovers = [input.slice(1, 3), input.slice(3, 5)];
+const compass = ['N', 'E', 'S', 'W'];
 
 rovers.forEach(function(rover) {
-  let x = Number(rover[0].split(' ')[0]);
-  let y = Number(rover[0].split(' ')[1]);
-  let startDirection = rover[0].split(' ')[2];
-  console.log("StartX: ", x);
-  console.log("StartY: ", y);
-  console.log("Start Direction: ", startDirection);
+  const startPosition = rover[0].split(' ');
+  let x = Number(startPosition[0]);
+  let y = Number(startPosition[1]);
+  const direction = startPosition[2];
+
+  const instructions = rover[1].split("");
+
+  let compassIndex = '';
+
+  for(i = 0; i < compass.length; i++) {
+    if (compass[i] === direction) {
+      compassIndex = i;
+    }
+  }
+
+  for(i = 0; i < instructions.length; i++) {
+    if (instructions[i] === 'L') {
+      compassIndex -= 1;
+      if (compassIndex < 0) {
+        compassIndex = 3;
+      }
+    } else if (instructions[i] === 'R') {
+      compassIndex += 1;
+      if (compassIndex === compass.length) {
+        compassIndex = 0
+      }
+    } else if (instructions[i] === 'M') {
+      if (compass[compassIndex] === 'N') {
+        y += 1;
+        if (y > maxPositionY) {
+          y = maxPositionY;
+        }
+      } else if (compass[compassIndex] === 'E') {
+        x += 1;
+        if (x > maxPositionX) {
+          y = maxPositionX;
+        }
+      } else if (compass[compassIndex] === 'S') {
+        y -= 1;
+        if (y < 0) {
+          y = 0;
+        }
+      } else if (compass[compassIndex] === 'W') {
+        x -= 1;
+        if (x < 0) {
+          x = 0;
+        }
+      }
+    }
+  }
+
+  let output = (x + ' ' + y + ' ' + compass[compassIndex]);
+  console.log(output);
+
 })
-
-//Finding a given rover's instructions.
-// let instructions = (rover) => {
-//   input[2].split("");
-// }
-
-// const compass = ['N', 'E', 'S', 'W'];
-
-// let compassIndex = '';
-
-// for(i = 0; i < compass.length; i++) {
-//   if (compass[i] === startDirection) {
-//     compassIndex = i;
-//   }
-// }
-
-
-// for(i = 0; i < instructions.length; i++) {
-//   if (instructions[i] === 'L') {
-//     compassIndex -= 1;
-//     if (compassIndex < 0) {
-//       compassIndex = 3;
-//     }
-//   } else if (instructions[i] === 'R') {
-//     compassIndex += 1;
-//     if (compassIndex === compass.length) {
-//       compassIndex = 0
-//     }
-//   } else if (instructions[i] === 'M') {
-//     if (compass[compassIndex] === 'N') {
-//       y += 1;
-//       if (y > maxPositionY) {
-//         y = maxPositionY;
-//       }
-//     } else if (compass[compassIndex] === 'E') {
-//       x += 1;
-//       if (x > maxPositionX) {
-//         y = maxPositionX;
-//       }
-//     } else if (compass[compassIndex] === 'S') {
-//       y -= 1;
-//       if (y < 0) {
-//         y = 0;
-//       }
-//     } else if (compass[compassIndex] === 'W') {
-//       x -= 1;
-//       if (x < 0) {
-//         x = 0;
-//       }
-//     }
-//   }
-// }
-
-// console.log("Final Position: ", x, y, compass[compassIndex]);
 
 module.exports = {};
