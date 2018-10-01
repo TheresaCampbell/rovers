0cba97ea522bef4b93a3bee2420a5d65a33b78cb
Theresa Campbell <t.campbell@sasktel.net>
2018-10-01 16:26:53 +0000

    Wrote tests that verify the x and y functions.

--- rovers.js
+++ rovers.js
@@ -9,6 +9,10 @@ const maxPositionY = input[0].split(' ')[1];
 const roverOne = input.slice(1, 3);
 const roverTwo = input.slice(3, 5);
 
+function count(arr) {
+  return arr.length;
+}
+
 let x = (rover) => {
   let xCoordinate = Number(rover[0].split(' ')[0]);
   return xCoordinate;
@@ -19,55 +23,57 @@ let y = (rover) => {
   return yCoordinate;
 }
 
-let startDirection = input[1].split(' ')[2];
+// let startDirection = input[1].split(' ')[2];
 
-const instructions = input[2].split("");
+// const instructions = input[2].split("");
 
-const compass = ['N', 'E', 'S', 'W'];
+// const compass = ['N', 'E', 'S', 'W'];
 
-let compassIndex = '';
+// let compassIndex = '';
 
-for(i = 0; i < compass.length; i++) {
-  if (compass[i] === startDirection) {
-    compassIndex = i;
-  }
-}
+// for(i = 0; i < compass.length; i++) {
+//   if (compass[i] === startDirection) {
+//     compassIndex = i;
+//   }
+// }
 
 
-for(i = 0; i < instructions.length; i++) {
-  if (instructions[i] === 'L') {
-    compassIndex -= 1;
-    if (compassIndex < 0) {
-      compassIndex = 3;
-    }
-  } else if (instructions[i] === 'R') {
-    compassIndex += 1;
-    if (compassIndex === compass.length) {
-      compassIndex = 0
-    }
-  } else if (instructions[i] === 'M') {
-    if (compass[compassIndex] === 'N') {
-      y += 1;
-      if (y > maxPositionY) {
-        y = maxPositionY;
-      }
-    } else if (compass[compassIndex] === 'E') {
-      x += 1;
-      if (x > maxPositionX) {
-        y = maxPositionX;
-      }
-    } else if (compass[compassIndex] === 'S') {
-      y -= 1;
-      if (y < 0) {
-        y = 0;
-      }
-    } else if (compass[compassIndex] === 'W') {
-      x -= 1;
-      if (x < 0) {
-        x = 0;
-      }
-    }
-  }
-}
+// for(i = 0; i < instructions.length; i++) {
+//   if (instructions[i] === 'L') {
+//     compassIndex -= 1;
+//     if (compassIndex < 0) {
+//       compassIndex = 3;
+//     }
+//   } else if (instructions[i] === 'R') {
+//     compassIndex += 1;
+//     if (compassIndex === compass.length) {
+//       compassIndex = 0
+//     }
+//   } else if (instructions[i] === 'M') {
+//     if (compass[compassIndex] === 'N') {
+//       y += 1;
+//       if (y > maxPositionY) {
+//         y = maxPositionY;
+//       }
+//     } else if (compass[compassIndex] === 'E') {
+//       x += 1;
+//       if (x > maxPositionX) {
+//         y = maxPositionX;
+//       }
+//     } else if (compass[compassIndex] === 'S') {
+//       y -= 1;
+//       if (y < 0) {
+//         y = 0;
+//       }
+//     } else if (compass[compassIndex] === 'W') {
+//       x -= 1;
+//       if (x < 0) {
+//         x = 0;
+//       }
+//     }
+//   }
+// }
 
-console.log("Final Position: ", x, y, compass[compassIndex]);
+// console.log("Final Position: ", x, y, compass[compassIndex]);
+
+module.exports = { x: x, y: y };
--- test/test_rovers.js
+++ test/test_rovers.js
@@ -1,4 +1,18 @@
-const chai = require("chai");
-const assert = chai.assert;
+const assert = require("chai").assert;
 const rovers = require("../rovers.js");
 
+const x = rovers.x;
+const y = rovers.y;
+const rover = ['3 5 E', 'LMMRMLMRRM'];
+
+describe('x', function() {
+  it('should return the x coordinate', function() {
+    assert.equal(x(rover), rover[0].split(' ')[0]);
+  });
+});
+
+describe('y', function() {
+  it('should return the y coordinate', function() {
+    assert.equal(y(rover), rover[0].split(' ')[1]);
+  });
+});
